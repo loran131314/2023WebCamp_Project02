4c8baa17bc13e85f69e2f1206f5c11b8c532af91
Laron <laron9486wk@gmail.com>
2023-06-03 20:40:17 +0800

    任務二修改_v1
    將校長直播的修改建議及助教給的建議做修正，並串接了 API 資料，不過 category 區塊的部分在串接資料時測試數次未果，所以此區塊目前是擱置狀態，但篩選及新舊排序按鈕的動態有完成。

--- Javascript/all.js
+++ Javascript/all.js
@@ -1,67 +1,264 @@
-// $(document).ready(function (event) {
+/*
+$(document).ready(function (event) {
 
-//     // 檢查視窗大小並執行相應程式碼
-//     checkWindowSize();
+    // 檢查視窗大小並執行相應程式碼
+    checkWindowSize();
 
-//     // 當視窗大小改變時重新檢查
-//     $(window).on('resize', function () {
-//         checkWindowSize();
-//     });
-// });
+    // 當視窗大小改變時重新檢查
+    $(window).on('resize', function () {
+        checkWindowSize();
+    });
+});
 
-// function checkWindowSize() {
-//     var windowWidth = $(window).width();
+function checkWindowSize() {
+    var windowWidth = $(window).width();
 
-//     if (windowWidth < 375) {
-//         // 裝置小於768px的程式碼
-//         console.log("裝置寬度小於768px");} else {
-//         // 裝置大於等於992px的程式碼
-//         console.log("裝置寬度大於等於992px");
-//     }
-// }
+    if (windowWidth < 375) {
+        // 裝置小於768px的程式碼
+        console.log("裝置寬度小於768px");} else {
+        // 裝置大於等於992px的程式碼
+        console.log("裝置寬度大於等於992px");
+    }
+}
+*/
 
 
 $(document).ready(function (event) {
 
-    $(window).width(function() {    // 此 Code 需要 F5 重新刷新才可使用（待解）
-        if ($('body').width() <= 376) {
-            $('#navbar').click(function () {
-                $('#navbar .menu-icon').toggle();
-                $('#navbar .close-icon').toggle();
-                $('#header ul').slideToggle();
-                $('#header .banner, .container, .footer .content, .footer .back-to-top').slideToggle();
-            });
-        }else if ($('body').width() > 376){
-            $('body').resize(function(){
-                location.reload();
-            });
-        }
-    });
-    $('#question1').click(function () {
-        $('#question1 .add').toggle();
-        $('#question1 .remove').toggle();
-        $('#question1 #answer').slideToggle();
-    });
-    $('#question2').click(function () {
-        $('#question2 .add').toggle();
-        $('#question2 .remove').toggle();
-        $('#question2 #answer').slideToggle();
-    });
+ $('.navbar-icon-mobile').click(function () {
+    $('.navbar-menu-icon-mobile').toggle();
+    $('.navbar-close-icon-mobile').toggle();
+    $('.navbar-menu-mobile').slideToggle();
+  });
 
-    $('#question3').click(function () {
-        $('#question3 .add').toggle();
-        $('#question3 .remove').toggle();
-        $('#question3 #answer').slideToggle();
-    });
-    $('#question4').click(function () {
-        $('#question4 .add').toggle();
-        $('#question4 .remove').toggle();
-        $('#question4 #answer').slideToggle();
-    });
-    $('#question5').click(function () {
-        $('#question5 .add').toggle();
-        $('#question5 .remove').toggle();
-        $('#question5 #answer').slideToggle();
-    });
+  $('.category-click').click(function () {
+    $(this).find('.category-menu').slideToggle();
+  });
+  $('.sort-new').click(function (event) {
+    event.preventDefault();
+    $('.sort-menu').slideDown();
+    $('.sort-text').text('由新到舊');
+  });
+  $('.sort-old').click(function (event) {
+    event.preventDefault();
+    $('.sort-menu').slideDown();
+    $('.sort-text').text('由舊到新');
+  });
 
+  $('.QnA-list').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('QnA-list-active');
+    $(this).find('.icon-add').toggleClass('d-none');
+    $(this).find('.icon-remove').toggle();
+    $(this).find('.QnA-detail-a').slideToggle();
+  })
 });
+
+const swiper = new Swiper('.swiper', {
+
+  // 頁面呈現的物件數
+  slidesPerView: 1,
+  // 一次切換幾個物件
+  slidesPerGroup: 1,
+  // 物件之間的間距,單位px,其他單位用字串呈現
+  spaceBetween: 0,
+  // 自動輪播,單位毫秒
+  autoplay: {
+    delay: 2000,
+  },
+  // 斷點
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 0,
+      spaceBetween: 24,
+    },
+  },
+  // 分頁   
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,        // 分頁按鈕可點選切換
+  },
+  // 左右箭頭
+  /*  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },*/
+  // 滾動條
+  /*  scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },*/
+});
+
+
+/* --------------- 利用穎旻老師的程式碼做修改 --------------- */
+// API 文件：https://hackmd.io/wA0Zeuk8QI-i7jeEXVssIA?view
+// 資料串接
+const apiPath = "https://2023-engineer-camp.zeabur.app";    // Line109
+const product = document.querySelector(".product");         // Line135
+const pagination = document.querySelector(".pagination");   // Line
+
+const data = {
+  type: "",
+  sort: 0,
+  page: 1,
+  search: ""
+};
+
+let worksData = [];     // Line138
+let pagesData = {};     // Line165
+
+// 若 API 文件內的 request／Query String 的 request 是 false 則 ? 問號後的路徑可以不寫
+function getData({ type, sort, page, search }) {
+  const apiUrl = `${apiPath}/api/v1/works?
+  sort=${sort}&
+  page=${page}&
+  ${type ? `type=${type}&` : ""}
+  ${search ? `search=${search}` : ""}
+  `;
+
+  // axios 取第三方資料
+  axios.get(apiUrl).then(function (response) {
+    worksData = response.data.ai_works.data;
+    pagesData = response.data.ai_works.page;
+
+    // console 確認資料取得
+    // console.log('response', response)
+    // console.log('worksData', worksData)
+    // console.log('pagesData', pagesData)
+
+    WorksRender();
+    PagesRender();
+  });
+}
+
+getData(data);  // Line97
+
+// 渲染作品
+function WorksRender() {
+  let works = "";
+
+  worksData.forEach(function (item) {
+    works += /*html*/
+      `<li class="product-card">
+        <div class="product-card-pic">
+          <img class="img" src="${item.imageUrl}" alt="ai image">
+        </div>
+        <div class="product-subject">
+          <h4 class="h4">${item.title}</h4>
+          <p class="product-subject-describe">${item.description}</p>
+        </div>
+        <div class="product-belong">
+          <p class="ai">AI 模型</p>
+          <p class="name">${item.discordId.slice(0, -5)}</p>
+        </div>
+        <div class="product-function">
+          <span class="tag">#${item.type}</span>
+          <a class="card-link" href="${item.link}" target="_blank">
+            <span class="material-icons">share</span>
+          </a>
+        </div>
+    </li>`;
+  });
+
+  product.innerHTML = works;   // Line 95
+}
+
+// 分頁切換
+function changePage(pagesData) {
+  const pageLinks = document.querySelectorAll("a.page-link");
+  let pageId = "";
+
+  pageLinks.forEach(function (item) {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      pageId = e.target.dataset.page;
+      data.page = Number(pageId);
+
+      if (!pageId) {
+        data.page = Number(pagesData.current_page) + 1;
+      }
+
+      getData(data);
+    });
+  });
+}
+
+/* --------------- 先撈老師的範例,之後要回頭看 Code --------------- */
+// 渲染分頁
+function PagesRender() {
+  let pageStr = "";
+
+  for (let i = 1; i <= pagesData.total_pages; i += 1) {
+    pageStr += /*html*/
+      `<li class="page-item ${pagesData.current_page == i ? "active" : ""}" >
+      <a class="
+      page-link ${pagesData.current_page == i ? "disabled" : ""}"
+      href="#"  
+      data-page="${i}">${i}</a>
+    </li>`;
+  }
+
+  if (pagesData.has_next) {
+    pageStr += /*html*/
+      `<li class="page-item">
+      <a class="page-link" href="#">
+        <span class="material-icons">keyboard_arrow_right</span>
+      </a>
+    </li>`;
+  }
+  pagination.innerHTML = pageStr;
+
+  changePage(pagesData);
+}
+
+// 排序切換
+const desc = document.querySelector("#desc");
+const asc = document.querySelector("#asc");
+const btnSort = document.querySelector("#btn-sort");
+//  由新到舊 -> sort = 0
+// desc.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   data.sort = 0;
+//   getData(data);
+//   btnSort.innerHTML = '<span class="category-text">由新到舊</span><span class="material-icons">expand_more</span>';
+// });
+//  由舊到新 -> sort = 1
+// asc.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   data.sort = 1;
+//   getData(data);
+//   btnSort.innerHTML = '<span class="category-text">由舊到新</span><span class="material-icons">expand_more</span>';
+// });
+
+// 類型切換
+// const filterBtns = document.querySelectorAll(".filter-btn li a");
+// filterBtns.forEach(function (item) {
+//   item.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if (item.textContent === "全部") {
+//       console.log(item.textContent);
+//       data.type = "";
+//     } else {
+//       console.log(item.textContent);
+//       data.type = item.textContent;
+//     }
+//     getData(data);
+//   });
+// });
+
+// 搜尋欄
+// const search = document.querySelector("#search");
+// search.addEventListener("keydown", function (e) {
+//   if (e.keyCode === 13) {
+//     data.search = search.value;
+//     data.page = 1;
+//     getData(data);
+//   }
+// });
